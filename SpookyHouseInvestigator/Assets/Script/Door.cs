65a2dee73aa24e55f0b632a0a1be6b4d496dc3e5
dereckmar <70959716+dereckmar@users.noreply.github.com>
2021-03-22 09:58:51 -0500

    Add files via upload

---
+++ SpookyHouseInvestigator/Assets/Script/Door.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool Open = false;
+    public bool Locked = false;
+    public static Door instance;
+    public bool playerClose = false;
+    public List<Transform> components;
+
+    public void Update()
+    {
+        if (Locked == false)
+        {
+            if (playerClose == true)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (Open == false)
+                    {
+                        foreach (Transform child in transform) child.gameObject.SetActive(false);
+                        Open = true;
+                    }
+                    else
+                    {
+                        foreach (Transform child in transform) child.gameObject.SetActive(true);
+                        Open = false;
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerClose = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerClose = false;
+        }
+    }
+
+    public void GotKey()
+    {
+        Locked = false;
+    }
+
+
+
+}
